173fb92c4d7977098c1d67e85a3b4e087dfade70
Maxim Sklyanin <maxsklyanin@gmail.com>
2024-04-29 19:58:40 +0300

    fib test

--- main/fib.py
+++ main/fib.py
@@ -1,12 +1,12 @@
-def fibonacci(n):
-  if n == 1 or n == 2:
-    return 1
-  else:
-    return fibonacci(n-1) + fibonacci(n-2)
-
-num_terms = int(input("Enter the number of terms: "))
-
-
-print("Fibonacci Sequence: ")
-for i in range(1, num_terms + 1):
-  print(fibonacci(i), end=" ")
+def fib(n):
+    if not isinstance(n, int) or (n < 1):
+        return None
+    if (n == 1):
+        return 0
+        
+    a = [0, 1] 
+      
+    for i in range(2, n): 
+        a.append(a[i-1] + a[i-2]) 
+    
+    return a[-1]
