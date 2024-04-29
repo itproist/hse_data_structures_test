a7ca79001989fec0d07e047930241771df155e41
Maxim Sklyanin <maxsklyanin@gmail.com>
2024-04-29 19:19:53 +0300

    first time

--- main/fib.py
+++ main/fib.py
@@ -1,15 +1,16 @@
-# Числа Фибоначчи — числовой ряд, при котором каждое последующее число равно сумме двух предыдущих
-
-# Получить число Фибоначчи по его номеру
 def fib(n):
     if not isinstance(n, int) or n < 0:
         return None
-    elif n == 0:
+    if n == 0:
         return None
     elif n == 1:
         return 0
+    elif n == 2:
+        return 1
     else:
-        a, b = 0, 1
+        a = 0
+        b = 1
         for _ in range(2, n):
-            a, b = b, a + a
-        return b if n >= 2 else 1
+            a, b = b, a + b
+        return b
+
