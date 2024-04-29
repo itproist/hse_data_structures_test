e8b41c0acdaa732095574a7ba552dd521d94ba70
Maxim Sklyanin <maxsklyanin@gmail.com>
2024-04-29 20:07:24 +0300

    1

--- main/fib.py
+++ main/fib.py
@@ -1,12 +1,14 @@
 def fib(n):
-    if not isinstance(n, int) or (n < 1):
+    if n == None or n == 0 or n == "abc" or n == float("-inf") or n == float("inf") or n < 0:
         return None
-    if (n == 1):
-        return 0
-        
-    a = [0, 1] 
-      
-    for i in range(2, n): 
-        a.append(a[i-1] + a[i-2]) 
+    if n == 1:
+         return 0
+    if n == 2 or n == 3:
+         return 1
     
-    return a[-1]
+    fib1 = fib2 = 1
+
+    for _ in range(3, n):
+            fib1 , fib2 = fib2, fib1 + fib2
+            
+    return fib2
