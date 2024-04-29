1335d6cbc2c21ffc596b932a977d49db0ca0cb6f
Maxim Sklyanin <maxsklyanin@gmail.com>
2024-04-29 20:04:19 +0300

    sort

--- main/sort.py
+++ main/sort.py
@@ -1,3 +1,8 @@
 
 def sort(a):
-    return
+    n = len(a)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if a[j] > a[j+1]:
+                a[j], a[j+1] = a[j+1], a[j]
+    return a
